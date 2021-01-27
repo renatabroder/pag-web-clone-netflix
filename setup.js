20fa2b9f9cd375b87cbec92f62d839ea62dc6423
Renata <renata.sendreti@gmail.com>
2021-01-26 22:31:18 -0300

    Responsividade para pagina web ok

--- setup.js
+++ setup.js
@@ -15,3 +15,48 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+$(document).on('click', function (e) {
+    if ($('#menu-nav').css('display') == 'none'
+    && $(e.target).closest("#menu-nav").length === 0
+    && $(e.target)[0] != $('.botao-menu-nav')[0]) {
+        $('#menu-nav').css('display', 'none');
+    }
+});
+
+menu = (() => {
+    let menuVisivel = $('#menu-nav').css('display') == 'none' ? false : true;
+
+    if(!menuVisivel){
+        $('#menu-nav').css('display', 'flex')
+    }
+    else{
+        $('#menu-nav').css('display', 'none')
+    } 
+})
+
+searchButton = (() =>{
+    let campoVisivel = $('#search-field').css('max-width') == '0px' ? false : true;
+    let valCampo = $('#search-field').val()
+
+    console.log(campoVisivel)
+    if(!campoVisivel){
+        $('#search-field').removeClass('hide');
+        $('.search-bar').removeClass('hide');
+    }
+    else if(campoVisivel && ( valCampo == '')){
+        $('#search-field').addClass('hide');
+        $('.search-bar').addClass('hide');    
+    }
+    else if(valCampo != ''){
+        pesquisa();
+    } 
+});
+
+
+$(document).on('click', function (e) {
+    if ($(e.target).closest(".search-bar").length === 0) {
+        $('#search-field').addClass('hide')
+        $('.search-bar').addClass('hide');
+    }
+});
